dc85ea7ce1bbce8e3b9491596b2168c1ee9e490a
Leah <eahlay@gmail.com>
2015-11-11 23:27:08 -0500

    first fully working version

--- digitspan.js
+++ digitspan.js
@@ -1,12 +1,21 @@
 var diplayNum;
 var truth;
-var guess;
 var index;
+var interval;
+var length;
 
+function initiate() {
+    length = 3;
+    iteration = 1;
+    wrong = 0;
+}
 
-function run(length) {
-    index = length
-    window.setInterval(function() {
+function run() {
+    index = length;
+    truth = "";
+    $("#startButton").hide();
+    $('#intro').trigger('showIteration', iteration);
+    interval = setInterval(function() {
 	nextDigit();
     }, 1000);
 }
@@ -16,15 +25,51 @@ function nextDigit() {
     if (index > 0) {
 	var digit = getRandomInt(1,10);
 	$('#digitDisplay').trigger('showDigit', digit);
-	/*window.setTimeout(function() {
-	    $('#digitDisplay').trigger('showDigit', "");
-	}, 100);*/
+	setTimeout(function() {
+	    $('#digitDisplay').trigger('showDigit', " ");
+	}, 500);
+	truth = truth.concat(digit);
 	index--;
     } else {
-	window.clearInterval();
+	$('#digitDisplay').trigger('showDigit', " ");
+	endloop();
+	clearInterval(interval);
     }
 }
 
+function endloop() {
+    $("#digitDisplay").hide();
+    $("#entry").show();
+    $('#intro').trigger('changeButton', "Next");
+}
+
+function calc() {
+    var guess = document.getElementById("guessbox").value;
+    $("#digitDisplay").show();
+    $("#entry").hide();
+    $('#guessbox').val('');
+    $("#startButton").show();
+    if (guess!=truth) {
+	wrong++;
+	$('#digitDisplay').trigger('showDigit', "Incorrect. You said " + guess + " but the answer was " + truth);
+    } else {
+	$('#digitDisplay').trigger('showDigit', "Correct!");
+    }
+    truth="";
+    if (wrong > 1) {
+	var score = length - 1;
+	$('#digitDisplay').trigger('showDigit', "Your digit span score is: " + score);
+	$("#startButton").hide();
+    }
+    iteration++;
+    if (iteration > 6) {
+	length ++;
+	iteration = 1;
+	wrong = 0;
+	$('#intro').trigger('showLength', length);
+    }
+    
+}
 
 
 // Returns a random integer between min (included) and max (excluded)
